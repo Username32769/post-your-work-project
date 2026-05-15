810cc6f7bad0a7901d5d884a2e98e4637e39dd21
root <root@localhost>
2026-05-15 10:09:14 +0000

    add user input for the city in the bikeshare_2.py source file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -17,6 +17,14 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    city = ""
+    while True: 
+        city = input("Choose a city (chicago, new york city, washington):")
+        city = city.lower()
+        if city in list(CITY_DATA.keys()):
+            break
+        else:
+            print("This city is not supported. Choose one from the list.")
 
 
     # get user input for month (all, january, february, ... , june)
